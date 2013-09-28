f6cfb3bff73013af7b4ca4a827ae6f1efea28b4c
kennethbortveit <kenneth.bortveit@gmail.com>
2013-09-28 22:43:52 +0200

    bootstraps on

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Bortveits</title>
+<meta charset="utf-8" />
+<meta type="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+</head>
+<body>
+<h1>Bortveits</h1>
+<script src="bootstrap/js/jquery.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
