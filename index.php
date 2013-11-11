de3bcf1d27cef548b143430819c2841e05e46614
kennethbortveit <kennth.bortveit@gmail.com>
2013-11-11 21:55:18 +0100

    Restructured files and made the index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html> <head>
+<title>Bortveits</title>
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+</head>
+<body>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
