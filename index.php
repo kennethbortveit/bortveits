cf7b00520c42ff68925420fcd84f3b30be28b409
kennethbortveit <kenneth.bortveit@gmail.com>
2014-08-23 02:19:40 +0200

    Started on the angular

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
-<html> <head>
-<title>Bortveits</title>
-<meta charset="utf-8" />
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+
+
+<html>
+<head>
+<title>Bortveit's</title>
+<meta charset="utf8" />
 </head>
 <body>
-<script src="js/jquery.js"></script>
-<script src="js/bootstrap.min.js"></script>
+<h1>Bortveit's</h1>
+
 </body>
+
 </html>
